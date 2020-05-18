5f99fb87d80530e2bcc2b1d39a1be44d317bdad8
Mich3l-Br1t0 <michelllbrito@hotmail.com>
2020-05-18 23:08:48 +0000

    Update Pacman.java, adicionado o metodo teleporte, que esta na classe jogo, correcao na movimentacao

--- Pekimen/src/Pacman.java
+++ Pekimen/src/Pacman.java
@@ -14,34 +14,27 @@ public class Pacman extends ObjetoJogo {
 
 	@Override
 	public void tick() {
-
-		if (x > 856)
-			x = 0;
-
-		if (x < 0)
-			x = 856;
-
-		if (y > 680)
-			y = 42;
-
-		if (y < 42)
-			y = 680;
+		x += Vx;
+		y += Vy;
+		//abrange os casos de teleporte
+		x = Jogo.teleporte(x, 0, 846);
+		y = Jogo.teleporte(y, 42, 680);
 	}
 
 	@Override
 	public void render(Graphics g) {
 		g.drawImage(textura, x, y, 20, 15, null);
 	}
-	
-	private void colisao() {
+
+	/*private void colisao() {
 		for(int i = 0; i < controle.objetos.size(); i++) {
 			ObjetoJogo tempObject = controle.objetos.get(i);
 			if (tempObject.getID() == ID.Parede) {
 				if(getBounds().intersects(tempObject.getBounds())) {
-					
+
 				}
 			}
 		}
-	}
+	}*/
 
 }
